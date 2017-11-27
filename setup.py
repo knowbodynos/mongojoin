83c628d69f2aaa859b0f61b9b05c1ab1aa5270c2
Ross Everett Altman <altman.ro@discovery2.neu.edu>
2017-11-27 01:35:27 -0500

    Changed name to mongojoin.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 
 from setuptools import setup;
 
-setup(name='mongolink',
+setup(name='mongojoin',
       version='1.0',
-      description='A package that allows a continuous non-blocking read of large batches of documents from a MongoDB database (remote or local), with some action performed on each batch.',
-      url='https://github.com/knowbodynos/mongolink',
+      description='A package that allows a continuous non-blocking read of large batches of documents joined from various collections in a MongoDB database (remote or local), with some action performed on each batch.',
+      url='https://github.com/knowbodynos/mongojoin',
       author='Ross Altman',
       author_email='knowbodynos@gmail.com',
       license='GPLv3',
-      packages=['mongolink'],
+      packages=['mongojoin'],
       zip_safe=False);
